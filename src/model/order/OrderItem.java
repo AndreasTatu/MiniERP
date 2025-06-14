0ae7ab1dbbff52e1f918071651eff810015b6b80
Tatumoto <tatu.teuschl@gmx.at>
2025-06-14 21:36:42 +0200

    Alle Model-Classes fertig. Nun gehe ich den DAO-Layer an.

--- src/model/order/OrderItem.java
+++ src/model/order/OrderItem.java
@@ -1,4 +1,72 @@
 package model.order;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class OrderItem {
+
+    private int orderItemID;
+    private int orderID;
+    private int productID;
+    private int quantity;
+    private BigDecimal unitPrice;
+
+    public OrderItem(int orderID, int productID, int quantity, BigDecimal unitPrice) {
+        this.orderID = orderID;
+        this.productID = productID;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+    public int getOrderItemID() {
+        return orderItemID;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return orderID == orderItem.orderID && productID == orderItem.productID;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderID, productID);
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "orderItemID=" + orderItemID +
+                ", orderID=" + orderID +
+                ", productID=" + productID +
+                ", quantity=" + quantity +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
 }
